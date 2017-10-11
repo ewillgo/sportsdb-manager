cd798e68601cc73addccc605f5181410ec677a63
liangwenwei <liangwenwei@tuandai.com>
2017-10-11 18:00:28 +0800

    Initial commit

---
+++ src/main/java/cc/sportsdb/manager/SportsdbManagerApplication.java
@@ -0,0 +1,12 @@
+package cc.sportsdb.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsdbManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsdbManagerApplication.class, args);
+	}
+}
