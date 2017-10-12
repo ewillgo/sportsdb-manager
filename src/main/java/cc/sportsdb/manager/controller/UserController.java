1cfedd6c7cde5afc38834b72d7aba825c490a63d
liangwenwei <liangwenwei@tuandai.com>
2017-10-12 10:14:15 +0800

    ...

--- src/main/java/cc/sportsdb/manager/controller/UserController.java
+++ src/main/java/cc/sportsdb/manager/controller/UserController.java
@@ -3,6 +3,10 @@ package cc.sportsdb.manager.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
@@ -12,5 +16,14 @@ public class UserController {
     public String login() {
 
         return "user/user-login";
+    }
+
+    @ResponseBody
+    @GetMapping("/json")
+    public Map<String, Object> json() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "hello");
+        map.put("age", 18);
+        return map;
     }
 }
