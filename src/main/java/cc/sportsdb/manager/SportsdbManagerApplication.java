1b405acf56bd49be29092f4d73f85343a396987a
liangwenwei <liangwenwei@tuandai.com>
2017-10-26 18:44:54 +0800

    ...

--- src/main/java/cc/sportsdb/manager/SportsdbManagerApplication.java
+++ src/main/java/cc/sportsdb/manager/SportsdbManagerApplication.java
@@ -1,13 +1,12 @@
 package cc.sportsdb.manager;
 
-import cc.sportsdb.common.test.AbstractApplicationTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class SportsdbManagerApplication extends AbstractApplicationTest {
+public class SportsdbManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SportsdbManagerApplication.class, args);
---
+++ src/test/java/cc/sportsdb/manager/controller/UserControllerTest.java
@@ -0,0 +1,6 @@
+package cc.sportsdb.manager.controller;
+
+import cc.sportsdb.manager.base.MockBaseControllerTest;
+
+public class UserControllerTest extends MockBaseControllerTest<UserController> {
+}
