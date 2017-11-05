33a6ed474374b1e4b0273aaf0394b897c7a1fac5
ewill.leung@foxmail.com <will89liangwenwei>
2017-11-05 22:30:22 +0800

    ...

--- document/sql/spdb_manager.sql
+++ document/sql/spdb_manager.sql
@@ -1,12 +1,16 @@
 create table spdb_manager (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
-  email VARCHAR(50) NOT NULL COMMENT '邮箱账号',
-  password VARCHAR(64) NOT NULL COMMENT '密码',
-  nickname VARCHAR(50) NULL COMMENT '昵称',
-  avatar VARCHAR(255) NULL COMMENT '头像地址',
-  create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  update_time DATETIME NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间'
-) DEFAULT CHARSET=utf8;
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Primary key',
+  manager_id CHAR(36) NOT NULL COMMENT 'Manager ID',
+  email VARCHAR(50) NOT NULL COMMENT 'Email account',
+  password VARCHAR(64) NOT NULL COMMENT 'Password',
+  nickname VARCHAR(50) NULL COMMENT 'Nickname',
+  avatar VARCHAR(255) NULL COMMENT 'Avatar path',
+  signin_time DATETIME NULL COMMENT 'Sign in time',
+  create_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Create time',
+  update_time DATETIME NULL ON UPDATE CURRENT_TIMESTAMP COMMENT 'Modified time',
+  UNIQUE KEY spdb_manager_unique_email(email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Manager table';
 
+ALTER TABLE spdb_manager ADD INDEX idx_spdb_manager_id (manager_id);
 ALTER TABLE spdb_manager ADD INDEX idx_spdb_manager_email (email, password);
 ALTER TABLE spdb_manager ADD INDEX idx_spdb_create_time (create_time);
